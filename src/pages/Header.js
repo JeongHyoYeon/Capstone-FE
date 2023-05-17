1027554ba4f6a282c3ce183c364f0a44ce27a897
oohyj <hyj0660@gmail.com>
2023-05-18 00:09:09 +0900

    헤더 수정

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,10 +3,13 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import Alarm from "../../src/components/common/Alarm";
 import BackButton from "../components/common/BackButton";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
-  if (window.location.pathname === "/") return null;
-  else if (window.location.pathname === "/group")
+  const location = useLocation();
+
+  if (location.pathname === "/") return null;
+  else if (location.pathname === "/group/")
     return (
       <div className="header-all">
         <div className="brand-Name">
